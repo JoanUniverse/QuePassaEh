d6554e8fa4a1c2dd3e5aed1d9d1e0a6c3257875e
JoanUniverse <jonnyy184@gmail.com>
2022-02-02 16:58:57 +0100

    Segona pàgina + solid login

--- js/script.js
+++ js/script.js
@@ -1,13 +1,20 @@
 var url = "http://webservers2w.westeurope.cloudapp.azure.com/quepassaeh/server/public/";
 const url_login = url + "login/";
+const url_missatges = url + "provamissatge/";
 var finestraMissatge;
 var finestraLogin;
+var token;
+var id_usuari;
+var benvinguda;
 
 window.onload = function () {
     finestraMissatge = document.getElementById("missatge");
     finestraLogin = document.getElementById("login");
-    var botoLogin = document.getElementById("loginButton");
-    botoLogin.addEventListener("click", comprovaLogin);
+    token = document.getElementById("token");
+    id_usuari = document.getElementById("idUsuari");
+    benvinguda = document.getElementById("benvinguda");
+    document.getElementById("loginButton").addEventListener("click", comprovaLogin);
+    //document.getElementById("missatgeButton").addEventListener("click", enviaMissatge);
 }
 
 function login(email, password){
@@ -27,6 +34,8 @@ function login(email, password){
         .then(
             json => {
                 if(json.correcta){
+                    token = json.dades.token;
+                    id_usuari = json.dades.codiusuari;
                     loginCorrecte(json);
                 } else{
                     alert("ERROR");
@@ -53,4 +62,36 @@ function loginCorrecte(dades){
     finestraLogin.style.display = "none";
     finestraMissatge.style.display = "block";
     console.log(dades);
+    benvinguda.innerHTML = "Benvingut " + dades.dades.nom;
+}
+
+function enviaMissatge(){
+    missatge = document.getElementById("missatgeInput").value.trim();
+    if(missatge != ""){
+        var formData = new FormData();
+        formData.append('codiusuari', id_usuari);
+        formData.append('msg', missatge);
+        fetch(url_login,{method: 'POST', body:formData}
+            )
+            .then(resposta => {
+                console.log(resposta);
+                if (resposta.ok) {
+                    return resposta.json();
+                } else {
+                    alert('Error: ' + resposta.status);
+                }
+            })
+            .then(
+                json => {
+                    if(json.correcta){
+                        //TODO
+                    } else{
+                        alert("ERROR");
+                    }
+                }
+            )
+            .catch(
+                error => console.log(error)
+            );
+    }
 }
